4ebfe060f80d510e819f98e3fff2901be18d8e83
Reham Hamdi <rehamhamdi53@gmail.com>
2026-09-15 23:10:00 +0300

    feat: add unified Response wrapper for API responses

---
+++ BackEnd/Veloura.Application/Common/Wrappers/Response.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace Veloura.Application.Common.Wrappers;
+
+public class Response<T>
+{
+    public T? Data { get; set; }
+    public bool Succeeded { get; set; }
+    public string? Message { get; set; }
+    public List<string>? Errors { get; set; }
+    public HttpStatusCode StatusCode { get; set; }
+}

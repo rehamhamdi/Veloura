fa6bf91a6db1ea15c2263b9b3d4f795ef0b5cc22
AmeraBakr <meraabadran3@gmail.com>
2026-09-15 14:47:57 +0300

    missed files

--- FrontEnd/src/main.tsx
+++ FrontEnd/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import './index.css'
-import App from './App.tsx'
+import App from './App'
 import { store } from './store/store'
 
 createRoot(document.getElementById('root')!).render(

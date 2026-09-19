73712e210be60ff9567c2876b8fa7416ec11d0f4
Reham Hamdi <rehamhamdi53@gmail.com>
2026-09-19 17:41:37 +0300

    fix: update CORS frontend origin

--- BackEnd/Veloura.API/Program.cs
+++ BackEnd/Veloura.API/Program.cs
@@ -88,7 +88,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend", policy =>
     {
         policy.WithOrigins(
-                "http://localhost:5173",
+                "http://localhost:5174",
                 "https://veloura-opal.vercel.app"
               )
               .AllowAnyHeader()
